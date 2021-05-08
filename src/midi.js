b6241999d22a9ad409fb0ae9b44bfb865932d545
Bela Terjek <bela@netc.hu>
2021-05-08 20:25:26 +0200

    Display midi note events on js console

--- src/midi.js
+++ src/midi.js
@@ -1,16 +1,37 @@
-navigator.requestMIDIAccess()
-    .then(onMIDISuccess, onMIDIFailure);
+navigator.requestMIDIAccess().then(midiSuccess, midiFailure);
 
-function onMIDISuccess(midiAccess) {
+function midiSuccess(midiAccess) {
     for (var input of midiAccess.inputs.values()) {
-        input.onmidimessage = getMIDIMessage;
+        input.onmidimessage = midiMessage;
     }
 }
 
-function getMIDIMessage(midiMessage) {
-    console.log(midiMessage);
+function midiFailure() {
+    console.log('Could not access your MIDI devices.');
 }
 
-function onMIDIFailure() {
-    console.log('Could not access your MIDI devices.');
+function midiMessage(message) {
+    var command = message.data[0];
+    var note = message.data[1];
+    var velocity = (message.data.length > 2) ? message.data[2] : 0;
+    switch (command) {
+        case 144:
+            if (velocity > 0) {
+                midiNoteOn(note, velocity);
+            } else {
+                midiNoteOff(note);
+            }
+            break;
+        case 128:
+            midiNoteOff(note);
+            break;
+    }
+}
+
+function midiNoteOn(note) {
+    console.log('on:' + note);
+}
+
+function midiNoteOff(note) {
+    console.log('off:' + note);
 }
