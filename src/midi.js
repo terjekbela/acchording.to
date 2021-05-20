d8983ecf3a1092c65b64586897264d97e16a890f
Bela Terjek <bela@netc.hu>
2021-05-21 00:53:32 +0200

    Simplify note placement

--- src/midi.js
+++ src/midi.js
@@ -15,12 +15,10 @@ navigator.requestMIDIAccess().then(
             const optionEl = document.createElement("option");
             optionEl.appendChild(document.createTextNode(o.name));
             selectEl.appendChild(optionEl);
-
         });
     },
     () => console.log('No MIDI devices.')
 );
-
 function midiInMessage(message) {
     var command = message.data[0];
     var note = message.data[1];
@@ -43,25 +41,23 @@ function notePlace() {
         e.parentElement.classList.remove('show');
         e.remove();
     });
-    notesArr.forEach((n,i,a) => {
-        notePlaceNote(n, (i > 0 || a.length<4)?'top':'bottom');
+    notesArr.forEach((note,i,a) => {
+        let staff = (i > 0 || a.length<4)?'top':'bottom';
+        let lineEl = null;
+        if (noteMatchStaff(note, 'top') && noteMatchStaff(note, 'bottom')) {
+            lineEl = noteMatchLine(note, staff);
+        } else if (noteMatchStaff(note, 'top')) {
+            lineEl = noteMatchLine(note, 'top');
+        } else if (noteMatchStaff(note, 'bottom')) {
+            lineEl = noteMatchLine(note, 'bottom');
+        }
+        if (lineEl!==null) {
+            let noteEl = document.createElement("note");
+            lineEl.appendChild(noteEl);
+            if (lineEl.classList.contains('ledger')) lineEl.classList.add('show');
+            if (lineEl.classList.contains('s' + note)) noteEl.classList.add('sharp');
+        }
     });
-}
-function notePlaceNote(note, staff) {
-    let lineEl = null;
-    if (noteMatchStaff(note, 'top') && noteMatchStaff(note, 'bottom')) {
-        lineEl = noteMatchLine(note, staff);
-    } else if (noteMatchStaff(note, 'top')) {
-        lineEl = noteMatchLine(note, 'top');
-    } else if (noteMatchStaff(note, 'bottom')) {
-        lineEl = noteMatchLine(note, 'bottom');
-    }
-    if (lineEl!==null) {
-        let noteEl = document.createElement("note");
-        lineEl.appendChild(noteEl);
-        if (lineEl.classList.contains('ledger')) lineEl.classList.add('show');
-        if (lineEl.classList.contains('s' + note)) noteEl.classList.add('sharp');
-    }
 }
 function noteMatchStaff(note, staff) {
     if(document.querySelectorAll('staff.' + staff + ' .n' + note).length) {
