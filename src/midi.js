f32a23f8a483f8184b5a5993a81bc67875eb6152
Bela Terjek <bela@netc.hu>
2021-05-21 18:32:54 +0200

    Show all ledger lines correctly (c2-c6)

--- src/midi.js
+++ src/midi.js
@@ -41,6 +41,10 @@ function notePlace() {
         e.parentElement.classList.remove('show');
         e.remove();
     });
+    document.querySelectorAll('line.ledger').forEach((e) => {  // ugly
+        e.classList.remove('show');
+    });
+    console.log(notes);
     notesArr.forEach((note,i,a) => {
         let staff = (i > 0 || a.length<4)?'top':'bottom';
         let lineEl = null;
@@ -56,6 +60,16 @@ function notePlace() {
             lineEl.appendChild(noteEl);
             if (lineEl.classList.contains('ledger')) lineEl.classList.add('show');
             if (lineEl.classList.contains('s' + note)) noteEl.classList.add('sharp');
+            if (lineEl.classList.contains('f' + note)) noteEl.classList.add('flat');
+            ledgerLines = Array.from(lineEl.classList).filter((c)=>{
+                return c.charAt(0)=='l' && c!='ledger';
+            });
+            if(ledgerLines.length) {
+                ledgerLines.forEach((lineClass)=>{
+                    let sel = '.n'+lineClass.substr(1,3);
+                    lineEl.parentElement.querySelector(sel).classList.add('show');
+                });
+            }
         }
     });
 }
@@ -63,6 +77,8 @@ function noteMatchStaff(note, staff) {
     if(document.querySelectorAll('staff.' + staff + ' .n' + note).length) {
         return true;
     } else if(document.querySelectorAll('staff.' + staff + ' .s' + note).length) {
+        return true;
+    } else if(document.querySelectorAll('staff.' + staff + ' .f' + note).length) {
         return true;
     }
     return false;
@@ -72,7 +88,7 @@ function noteMatchLine(note, staff) {
     lineElSharp   = document.querySelector('staff.' + staff + ' .s' + note);
     lineElFlat    = document.querySelector('staff.' + staff + ' .f' + note);
     if (lineElNatural!==null) return lineElNatural;
-    if (lineElSharp!==null) return lineElSharp;
     if (lineElFlat!==null) return lineElFlat;
+    if (lineElSharp!==null) return lineElSharp;
     return null;
 }
