dee9a6a90931f582d556843d5e56e5b3b7abf1b1
Bela Terjek <bela@netc.hu>
2021-05-23 16:44:37 +0200

    Reconnect midi inputs when they appear

--- src/midi.js
+++ src/midi.js
@@ -1,25 +1,15 @@
 let notes = new Set;
 
+// runs one on page load and connects all midi inputs
 navigator.requestMIDIAccess().then(
     (midi) => {
-        [...midi.inputs.values()].forEach((i)=>{
-            i.onmidimessage = midiInMessage;
-            const selectEl = document.querySelectorAll('midiport select optgroup')[0];
-            const optionEl = document.createElement("option");
-            optionEl.appendChild(document.createTextNode(i.name));
-            selectEl.appendChild(optionEl);
-        });
-        [...midi.outputs.values()].forEach((o)=>{
-            o.onmidimessage = midiOutMessage;
-            const selectEl = document.querySelectorAll('midiport select optgroup')[1];
-            const optionEl = document.createElement("option");
-            optionEl.appendChild(document.createTextNode(o.name));
-            selectEl.appendChild(optionEl);
-        });
+        [...midi.inputs.values()].forEach((i)=>midiConnectInput(i));
+        midi.onstatechange = midiOnStateChange
     },
-    () => console.log('No MIDI devices.')
+    (err) => console.log('No MIDI devices.')
 );
 
+// receives midi messages, stores the in the notes Set and invokes note display
 function midiInMessage(message) {
     var command = message.data[0];
     var note = message.data[1];
@@ -27,12 +17,30 @@ function midiInMessage(message) {
     switch (command) {
         case 144:
             velocity > 0 ? notes.add(note) : notes.delete(note);
+            notePlace();
             break;
         case 128:
             notes.delete(note);
+            notePlace();
             break;
     }
-    notePlace();
 }
 
-function midiOutMessage(message) {}
+// cycles through all inputs on midi state change (user plugges midi keyboard in)
+function midiOnStateChange(event) {
+    const selectEl = document.querySelector('midiport select');
+    while (selectEl.firstChild) selectEl.removeChild(selectEl.lastChild);
+    const optionEl = document.createElement("option");
+    optionEl.appendChild(document.createTextNode('MIDI'));
+    selectEl.appendChild(optionEl);
+    [...event.target.inputs.values()].forEach((i)=>midiConnectInput(i));
+}
+
+// connects to the midi input received
+function midiConnectInput(input) {
+    input.onmidimessage = midiInMessage;
+    const selectEl = document.querySelector('midiport select');
+    const optionEl = document.createElement("option");
+    optionEl.appendChild(document.createTextNode(input.name));
+    selectEl.appendChild(optionEl);
+}
