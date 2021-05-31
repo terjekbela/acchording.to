8ba44fbce25b34d75d3c564423061f54f3c1a354
Bela Terjek <bela@netc.hu>
2021-05-31 08:18:16 +0200

    Format midi key display

--- src/Keys.js
+++ src/Keys.js
@@ -4,10 +4,11 @@ import './Keys.css';
 export default function Keys(props) {
     // eslint-disable-next-line
     const { notes, ...restProps } = props;
+    const keys = [...notes].sort((a, b) => a - b).join(', ')
 
     return (
         <div key="123" className="keys">
-            MIDI keys: { notes }
+            {keys}
         </div>
     )
 }
