26be143164feaebb9f547fc9b8fa874c1121cd8e
Bela Terjek <bela@netc.hu>
2021-06-05 10:22:31 +0200

    Bugfix midi note release

--- src/App.js
+++ src/App.js
@@ -25,19 +25,17 @@ export default function App() {
         let command = message.data[0]
         let note = message.data[1]
         let velocity = (message.data.length > 2) ? message.data[2] : 0;
-        if(command===144 || (command===128 && velocity===0)){
+        if(command===144){
             setNotes((n) => {
-                if(!n.midi.has(note)) return {midi: new Set(n.midi).add(note)}
-                return n
+                if(n.midi.has(note)) return n
+                return {midi: new Set(n.midi).add(note)}
             })
-        } else if (command===128) {
+        } else if (command===128 || (command===144 && velocity===0)) {
             setNotes((n) => {
-                if(n.midi.has(note)) {
-                    let midi = new Set(n.midi)
-                    midi.delete(note)
-                    return {midi}
-                }
-                return n
+                if(!n.midi.has(note)) return n
+                let midi = new Set(n.midi)
+                midi.delete(note)
+                return {midi}
             })
         }
     }
